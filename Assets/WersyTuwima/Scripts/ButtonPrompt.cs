3726e7eb56388901d6bc72c0bfc096f7b6d30568
GraWersyTuwima <wersytuwima@notro.me>
2024-10-11 13:07:26 +0200

    Add button prompt minigame

---
+++ Assets/WersyTuwima/Scripts/ButtonPrompt.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ButtonPrompt : MonoBehaviour
+{
+    private TextMeshProUGUI _text;
+    private Animator _animator;
+
+    private ButtonPromptLetter[] _letters = new ButtonPromptLetter[]
+    {
+        new() { Letter = "Q", Key = KeyCode.Q },
+        new() { Letter = "W", Key = KeyCode.W },
+        new() { Letter = "E", Key = KeyCode.E },
+        new() { Letter = "R", Key = KeyCode.R },
+        new() { Letter = "A", Key = KeyCode.A },
+        new() { Letter = "S", Key = KeyCode.S },
+        new() { Letter = "D", Key = KeyCode.D },
+        new() { Letter = "F", Key = KeyCode.F },
+        new() { Letter = "Z", Key = KeyCode.Z },
+        new() { Letter = "X", Key = KeyCode.X },
+        new() { Letter = "C", Key = KeyCode.C },
+        new() { Letter = "V", Key = KeyCode.V }
+    };
+
+    private ButtonPromptLetter _currentLetter;
+
+    public float TimeToPressSeconds = 2f;
+    private float _timeToPress;
+    private bool _isActive;
+
+    private void Awake()
+    {
+        _text = GetComponentInChildren<TextMeshProUGUI>();
+        _animator = GetComponent<Animator>();
+    }
+
+    public void ShowPrompt()
+    {
+        if (_isActive) return;
+
+        _isActive = true;
+        _currentLetter = _letters[Random.Range(0, _letters.Length)];
+        _text.text = _currentLetter.Letter;
+        _timeToPress = Time.time + TimeToPressSeconds;
+        _animator.SetTrigger("Show");
+    }
+
+    public void HidePrompt()
+    {
+        _animator.SetTrigger("Hide");
+        ResetPrompt();
+    }
+
+    public void FailPrompt()
+    {
+        if (!_isActive) return;
+
+        _animator.SetTrigger("Fail");
+        ResetPrompt();
+    }
+
+    private void ResetPrompt()
+    {
+        _currentLetter = null;
+        _isActive = false;
+        _timeToPress = 0f;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Insert))
+        {
+            ShowPrompt();
+        }
+
+        if (!_isActive || _currentLetter == null) return;
+
+
+        if (Input.GetKeyUp(_currentLetter.Key))
+        {
+            HidePrompt();
+            return;
+        }
+        if (Time.time > _timeToPress)
+        {
+            FailPrompt();
+            return;
+        }
+
+        foreach (var letter in _letters)
+        {
+            if (letter.Key != _currentLetter.Key && Input.GetKeyUp(letter.Key))
+            {
+                FailPrompt();
+                break;
+            }
+        }
+    }
+}
+
+public record ButtonPromptLetter
+{
+    public string Letter { get; set; }
+    public KeyCode Key { get; set; }
+}
