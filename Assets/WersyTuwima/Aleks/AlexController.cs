dfb387341d3252260e81f66ca1a1e70bac8ead3c
GraWersyTuwima <wersytuwima@notro.me>
2024-10-14 22:07:29 +0200

    Add footstep sounds

--- Assets/WersyTuwima/Aleks/AlexController.cs
+++ Assets/WersyTuwima/Aleks/AlexController.cs
@@ -7,16 +7,26 @@ public class AlexController : MonoBehaviour
     [SerializeField]
     private float _speed = 5.0f;
 
+    [SerializeField]
+    private AudioClip[] _grassFootstepSounds;
+
+    [SerializeField]
+    private AudioClip[] _planksFootstepSounds;
+
     private float _horizontalInput;
     private float _verticalInput;
 
     private Rigidbody2D _rb;
     private Animator _animator;
 
+    private bool _isRunning;
+
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
+
+        StartCoroutine(PlayFootstepSound());
     }
 
     void Update()
@@ -25,7 +35,9 @@ public class AlexController : MonoBehaviour
         _verticalInput = Input.GetAxis("Vertical");
 
         Flip();
-        _animator.SetBool("IsRunning", _rb.velocity.magnitude > 0.1f);
+
+        _isRunning = _rb.velocity.magnitude > 0.1f;
+        _animator.SetBool("IsRunning", _isRunning);
     }
 
     private void FixedUpdate()
@@ -46,5 +58,34 @@ public class AlexController : MonoBehaviour
     {
         if (_horizontalInput == 0) return;
         transform.localScale = new Vector3(Mathf.Sign(_horizontalInput), 1, 1);
+    }
+
+    private IEnumerator PlayFootstepSound()
+    {
+        while (true)
+        {
+            if (_isRunning)
+            {
+                RaycastHit2D hit = Physics2D.Raycast(transform.position - new Vector3(0, 1.7f, 0),
+                    Vector2.down, 0.1f, LayerMask.GetMask("Ground"));
+
+                if (hit.collider == null)
+                {
+                    yield return null;
+                    continue;
+                }
+
+                AudioClip[] footstepSounds = hit.collider.CompareTag("Grass") ? _grassFootstepSounds : _planksFootstepSounds;
+
+                int randomIndex = Random.Range(0, footstepSounds.Length);
+                AudioManager.Instance.PlaySound(footstepSounds[randomIndex]);
+
+                yield return new WaitForSeconds(0.35f);
+            }
+            else
+            {
+                yield return null;
+            }
+        }
     }
 }
