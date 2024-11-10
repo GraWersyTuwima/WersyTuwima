2bcd5afa9aa8e9bbe34ed5b9efb1da78ffb8d465
GraWersyTuwima <wersytuwima@notro.me>
2024-11-10 11:08:45 +0100

    Add house interior

--- Assets/WersyTuwima/Scripts/HouseDoor.cs
+++ Assets/WersyTuwima/Scripts/HouseDoor.cs
@@ -3,8 +3,18 @@ using UnityEngine;
 
 public class HouseDoor : InteractableObject
 {
+    [SerializeField]
+    private bool _isInside = false;
+
     protected override void Interact()
     {
-        
+        if (_isInside)
+        {
+            Debug.Log("You are inside the house");
+        }
+        else
+        {
+            Debug.Log("You are outside the house");
+        }
     }
 }
