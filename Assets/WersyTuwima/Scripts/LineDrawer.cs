1b302e771ff5a0d2fca1ab5fa4ae7b53ed2ec796
NotroDev <wersytuwima@notro.me>
2024-11-27 19:24:08 +0100

    Improve the sewing minigame

--- Assets/WersyTuwima/Scripts/LineDrawer.cs
+++ Assets/WersyTuwima/Scripts/LineDrawer.cs
@@ -1,62 +1,117 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System.Linq;
 
 public class LineDrawer : MonoBehaviour
 {
-    public LineRenderer lineRenderer;
-    public float minDistance = 0.1f;
+    [SerializeField] private LineRenderer _lineRenderer;
+    [SerializeField] private ShirtOutlineChecker _outlineChecker;
 
-    private List<Vector3> points = new();
+    [SerializeField] private float _minDistance = 0.1f;
+    [SerializeField] private float _completionThreshold = 0.9f;
 
-    public ShirtOutlineChecker outlineChecker;
-    private bool isValid = true;
+    [SerializeField] private AudioClip _failSound;
 
-    void Start()
+    private List<Vector3> _points = new();
+
+    private bool _isValid = true;
+
+    private void Start()
     {
-        lineRenderer.positionCount = 0;
-
+        _lineRenderer.positionCount = 0;
     }
-    void Update()
+
+    private void Update()
     {
         if (Input.GetMouseButton(0))
         {
             Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             mousePosition.z = 0;
 
-            if (outlineChecker.IsPointValid(mousePosition))
+            if (_outlineChecker.IsPointValid(mousePosition))
             {
-                if (points.Count == 0 || Vector3.Distance(points[^1], mousePosition) > minDistance)
+                if (_points.Count == 0 || Vector3.Distance(_points[^1], mousePosition) > _minDistance)
                 {
-                    points.Add(mousePosition);
-                    lineRenderer.positionCount = points.Count;
-                    lineRenderer.SetPosition(points.Count - 1, mousePosition);
+                    _points.Add(mousePosition);
+                    _lineRenderer.positionCount = _points.Count;
+                    _lineRenderer.SetPosition(_points.Count - 1, mousePosition);
                 }
             }
             else
             {
-                isValid = false;
+                CheckCompletion();
                 ResetDrawing();
             }
         }
         else if (Input.GetMouseButtonUp(0))
         {
-            if (isValid)
+            if (_isValid)
             {
                 CheckCompletion();
+                AudioManager.Instance.PlaySound(_failSound);
             }
-            points.Clear();
-            lineRenderer.positionCount = 0;
+            ResetDrawing();
         }
     }
 
-    void ResetDrawing()
+    private void ResetDrawing()
     {
-        points.Clear();
-        lineRenderer.positionCount = 0;
+        _points.Clear();
+        _lineRenderer.positionCount = 0;
+        _isValid = true;
     }
 
-    void CheckCompletion()
+    private void CheckCompletion()
     {
-        Debug.Log("Rysowanie zakoñczone!");
+        if (_points.Count < 2) return;
+
+        float totalOutlineLength = CalculateTotalOutlineLength();
+        float coveredLength = CalculateCoveredLength();
+
+        float coverage = coveredLength / totalOutlineLength;
+        float realCoverage = coverage / 0.065f; // I hate this magic number, but I have no time to make more logical calculations...
+
+        if (realCoverage >= _completionThreshold)
+        {
+            Debug.Log($"Gratulacje! Obrysowa³eœ koszulkê w {realCoverage * 100:F2}%");
+        }
+        else
+        {
+            AudioManager.Instance.PlaySound(_failSound);
+        }
+    }
+
+    private float CalculateTotalOutlineLength()
+    {
+        Vector2[] outlinePoints = _outlineChecker.shirtOutline.points;
+        float totalLength = 0f;
+
+        for (int i = 0; i < outlinePoints.Length; i++)
+        {
+            Vector2 current = outlinePoints[i];
+            Vector2 next = outlinePoints[(i + 1) % outlinePoints.Length];
+            totalLength += Vector2.Distance(current, next);
+        }
+
+        return totalLength;
+    }
+
+    private float CalculateCoveredLength()
+    {
+        if (_points.Count < 2) return 0f;
+
+        float coveredLength = 0f;
+        for (int i = 0; i < _points.Count - 1; i++)
+        {
+            Vector2 start = _points[i];
+            Vector2 end = _points[i + 1];
+
+            if (_outlineChecker.IsPointValid(start) && _outlineChecker.IsPointValid(end))
+            {
+                coveredLength += Vector2.Distance(start, end);
+            }
+        }
+
+        return coveredLength;
     }
 }
--- Assets/WersyTuwima/Scripts/ShirtOutlineChecker.cs
+++ Assets/WersyTuwima/Scripts/ShirtOutlineChecker.cs
@@ -3,11 +3,10 @@ using UnityEngine;
 public class ShirtOutlineChecker : MonoBehaviour
 {
     public PolygonCollider2D shirtOutline;
-    public float tolerance = 0.5f;
 
     public bool IsPointValid(Vector2 point)
     {
         Vector2 closestPoint = shirtOutline.ClosestPoint(point);
-        return Vector2.Distance(point, closestPoint) <= tolerance;
+        return Vector2.Distance(point, closestPoint) < 0.05f;
     }
 }
