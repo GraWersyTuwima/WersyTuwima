77451cf8ae71dea810456e326aaa9b16dde0a658
NotroDev <wersytuwima@notro.me>
2024-11-30 11:02:55 +0100

    Change the introduction note

--- Assets/WersyTuwima/Scripts/Notebook.cs
+++ Assets/WersyTuwima/Scripts/Notebook.cs
@@ -34,7 +34,7 @@ public class Notebook : MonoBehaviour
     public Dictionary<Note, string> Notes { get; } = new()
     {
         { Note.Pusta, "Tyle tu miejsca!\nWidzê w oddali... Niæ do szycia?\n\"Krawiec szyje ubrania\"..." },
-        { Note.Wprowadzenie, "Witaj w grze <i>Wersy Tuwima</i>!\nGrasz jako Aleks – ch³opiec, który pragnie\njak najlepiej zapoznaæ siê z twórczoœci¹\nJuliana Tuwima. Podczas gry bêdziesz\neksplorowa³ miejsca opisane w wierszach\npoety i bra³ udzia³ w grach inspirowanych\njego utworami!\nMo¿esz otwieraæ ten notatnik klikaj¹c we\nwskaŸnik postêpu w prawym górnym rogu.\n\nMi³ej gry!" },
+        { Note.Wprowadzenie, "Witaj w grze <i>Wersy Tuwima</i>!\nGrasz jako Aleks – ch³opiec, który pragnie\njak najlepiej zapoznaæ siê z twórczoœci¹\nJuliana Tuwima. Podczas gry bêdziesz\neksplorowa³ miejsca opisane w wierszach\npoety i bra³ udzia³ w grach inspirowanych\njego utworami!\nMo¿esz otwieraæ ten notatnik klikaj¹c we\nwskaŸnik postêpu w prawym górnym rogu.\nZagl¹daj tu od czasu do czasu!\nMi³ej gry!" },
         { Note.Okulary, "Gdzie one mog¹ byæ?\n\nTa stara szafa...\nPianino w domu?\nMyszy tutaj chodz¹...\nTo lustro... wygl¹da dziwnie." },
         { Note.Koniec, "Koniec gry" },
 
